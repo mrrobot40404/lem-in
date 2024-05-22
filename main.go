f1a195114d6300775031cb7bf5ae92f9040cba1d
Zeyad Al Sakhi <mr.zeyadalsakhi@gmail.com>
2024-05-22 18:59:53 +0300

    parsing complete

--- main.go
+++ main.go
@@ -4,24 +4,24 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
 type Room struct {
 	name string // Room Identifier (Could be digit/string/whatever)
 	x, y int // Coordinates for visualization
-	visited bool // If visited already, then ignore on second pass of bfs exploration
-	path []*Room // Rooms linking to this room struct 
+	links []*Room // Rooms linking to this room struct 
 }
 
-type Path struct {
-	roomsInPath []*Room // List of rooms in this path
-	numberOfAnts int // Number of ants in the
-}
+// type Path struct {
+// 	roomsInPath []*Room // List of rooms in this path
+// 	antsInPath int // Number of ants in the
+// }
 
 type AntFarm struct {
-	rooms map[string]*Room
-	paths []*Path
+	rooms map[string]*Room // If visited already, then ignore on second pass of bfs exploration
+	numberOfAnts int // Number of ants in the ant farm
 	startRoom, endRoom *Room
 }
 
@@ -51,6 +51,14 @@ func parseFile(filepath string) (*AntFarm, error) {
 		if line == "" { // If no contents in the line
 			continue
 		} 
+
+		if antFarm.numberOfAnts == 0 {
+			antFarm.numberOfAnts, err = strconv.Atoi(line)
+			if err != nil {
+				return nil, fmt.Errorf("Invalid number of ants")
+			}	
+			continue
+		}
 		
 		if strings.HasPrefix(line, "#") { // If the line has a prefix with # we check if its the pending Room type either start or end
 			if line == "##start" {
@@ -61,12 +69,55 @@ func parseFile(filepath string) (*AntFarm, error) {
 			continue // We skip if we have comments or anything that is not a starting or ending room
 		}
 
-		if antFarm.
+		// Parsing Rooms
+		if strings.Contains(line, " ") {
+			parts := strings.Split(line, " ") // []string
+			if len(parts) != 3 {
+				return nil, fmt.Errorf("Invalid room format")
+			}
+
+			// Parse to store coordinates for bonus repository 
+			x, _ := strconv.Atoi(parts[1]) // Coordinate X
+			y, _ := strconv.Atoi(parts[2]) // Coordinate Y
+
+			room := &Room{
+				name: parts[0],
+				x: x,
+				y: y,
+				links: []*Room{},
+			}
+
+			antFarm.rooms[room.name] = room // The room name identifies the room struct for O(1) lookup
+
+			if pendingType == "start" {
+				antFarm.startRoom = room // If the pendingType is populated with "start", then the room after that line directly is the start room
+				pendingType = ""
+			} else if pendingType == "end" { // If the pendingType is populated with "end", then the room after that line directly is the end room
+				antFarm.endRoom = room
+				pendingType = ""
+			}
+		}
+		
+		// Parsing Links
+		if strings.Contains(line, "-") {
+			parts := strings.Split(line, "-")
+			if len(parts) != 2 {
+				return nil, fmt.Errorf("Invalid link format")
+			}
+			room1 := antFarm.rooms[parts[0]]
+			room2 := antFarm.rooms[parts[1]]
+			room1.links = append(room1.links, room2)
+			room2.links = append(room2.links, room1)
+		}
+	}	
+	
+	// Error: Hits an end of file condition without reading any data
+	err = scanner.Err()
+	if err != nil {
+		return nil, fmt.Errorf("Scanner Error")
 	}	
 
-
-
-	// Ant Farm does not have a start or end room
+	// Error: Ant Farm does not have a start or end room
 
 	if antFarm.startRoom == nil || antFarm.endRoom == nil {
 		return nil, fmt.Errorf("Missing Start Room or End Room")
